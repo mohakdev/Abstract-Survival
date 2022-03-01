4fdeac1b87ee76f1854326671192f64797d2478f
Mohak Jain <mohakjain55055@gmail.com>
2022-03-01 12:38:12 +0530

    ScoreAdded

--- Abstract Survival/Assets/Scripts/BulletScript.cs
+++ Abstract Survival/Assets/Scripts/BulletScript.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class BulletScript : MonoBehaviour
 {
     private Rigidbody2D bulletBody;
+    private AudioSource audioPlayer;
     public int Speed;
     private void Awake()
     {
         bulletBody = GetComponent<Rigidbody2D>();
+        audioPlayer = GetComponent<AudioSource>();
     }
     public void Shoot(Vector2 direction)
     {
         bulletBody.AddForce(direction * Speed);
+        audioPlayer.Play();
     }
     private void OnCollisionEnter2D(Collision2D other)
     {
---
+++ Abstract Survival/Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour
+{
+    public float Score;
+    public int IncrementPerFrame;
+    Text ScoreLabel;
+    private void Awake()
+    {
+        ScoreLabel = GetComponent<Text>();
+    }
+    private void Update()
+    {
+        Score += IncrementPerFrame * Time.deltaTime;
+        int DisplayScore = ((int)Score);
+        ScoreLabel.text = "SCORE : " + DisplayScore;
+    }
+    public void SetHighScore()
+    {
+        int Highscore = PlayerPrefs.GetInt("HS", 0);
+        if (Highscore < Score)
+        {
+            PlayerPrefs.SetInt("HS", ((int)Score));
+        }
+    }
+}
