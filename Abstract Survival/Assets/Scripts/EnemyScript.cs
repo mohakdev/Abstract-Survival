b9b5d1ed26bec447aec056eb6b8daf5c683baffb
Mohak Jain <mohakjain55055@gmail.com>
2022-03-02 13:48:12 +0530

    EnemyCompleted

--- Abstract Survival/Assets/Scripts/EnemyScript.cs
+++ Abstract Survival/Assets/Scripts/EnemyScript.cs
@@ -16,6 +16,9 @@ public class EnemyScript : MonoBehaviour
     }
     private void Update()
     {
+        Vector3 dir = Player.transform.position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg + 270;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         if (Vector2.Distance(transform.position, Player.transform.position) > StoppingDistance)
         {
             transform.position = Vector2.MoveTowards(transform.position, Player.transform.position, Speed * Time.deltaTime);
